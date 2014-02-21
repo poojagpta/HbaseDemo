d12c5e91f37cf3c6b4835e7f61d5771f56d985da
pooja.gpta <pooja.gupta@jbksoft.com>
2014-02-21 08:04:07 -0800

    first commit

---
+++ src/main/java/com/jbksoft/hadoop/App.java
@@ -0,0 +1,13 @@
+package com.jbksoft.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
